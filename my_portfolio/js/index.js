6dd4f44f3d0d69723dc816c66337e48e7eb24262
SGTTW <88731480+SGTTW@users.noreply.github.com>
2024-01-08 12:06:52 +0100

    first commit

---
+++ my_portfolio/js/index.js
@@ -0,0 +1,10 @@
+console.log("now it's working and on git!");
+
+const number = 50;
+const name = "deecode";
+
+const info = {
+  number: number,
+  name: name,
+};
+console.log(number, name, info);
